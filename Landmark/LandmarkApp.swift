40e86a577a65bf0b0575b70ddabe6a5b86ea6db0
syadool <takerumorimoto0417@gmail.com>
2022-12-26 12:23:25 +0900

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by 森元豪琉 on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
